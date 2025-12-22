8b64c5ba25bb074e6ccadf48987125cdcfc0991e
bakha <bakha@gmail.com>
2025-12-22 19:14:00 +0500

    first commit

---
+++ src/resturant/Order.java
@@ -0,0 +1,39 @@
+package resturant;
+
+public class Order {
+    private int place;
+    private int check;
+    public Order(int place, int check) {
+        this.place = place;
+        this.check = check;
+    }
+    public int getPlace() {
+        return place;
+    }
+    public void setPlace(int place) {
+        this.place = place;
+    }
+    public int getCheck() {
+        return check;
+    }
+    public void setCheck(int check) {
+        this.check = check;
+
+    }
+
+
+    public void discount() {
+        if (check >= 10000) {
+            int oldCheck = check;
+            check = check - check / 10;
+
+            System.out.println(
+                    "Order{place - " + place + ", check - " + oldCheck + "} with discount " + check
+            );
+        } else {
+            System.out.println(
+                    "Order{place - " + place + ", check - " + check + "} no discount"
+            );
+        }
+    }
+}
---
+++ src/resturant/Restaurant.java
@@ -0,0 +1,68 @@
+package resturant;
+
+public class Restaurant {
+
+    private String location;
+    private String title;
+    private float quantity;
+
+    public Restaurant(String location, String title ,  float quantity) {
+        this.location = location;
+        this.title = title;
+        this.quantity = quantity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setPrice(String place, float quantity) {
+        this.title = place;
+    }
+
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+
+public void info()
+{
+    System.out.println("Location: " + this.location);
+    System.out.println("Average bill: " + this.title);
+    System.out.println("Quantity of restaurant: " + this.quantity + "\n");
+}
+
+public void compare_restaurant(Restaurant that)
+{
+    if (this.quantity < that.quantity) {
+        System.out.println( this + " quantity less than " + that + " quantity");
+    }
+    else if (this.quantity > that.quantity) {
+        System.out.println( this + " quantity more than " + that + " quantity");
+    }
+    else
+    {
+        System.out.println("Quantities are the same" + "\n");
+    }
+}
+
+    @Override
+    public String toString() {
+        return  title + " " +   quantity ;
+    }
+
+}
+
